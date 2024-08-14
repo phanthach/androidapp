feecb91ea4632a8dca7dc51809d33b71e3816fdf
Phan Tuấn Thạch - B20DCCN635 <98653656+phanthach@users.noreply.github.com>
2024-08-14 16:16:30 +0700

    commit test

---
+++ app/src/main/java/com/example/apptest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.apptest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.apptest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
